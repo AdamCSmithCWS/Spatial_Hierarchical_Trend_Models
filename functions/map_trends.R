160358a1d7a3fcb19a5675f96e353378ea916328
L-Daly <129316408+L-Daly@users.noreply.github.com>
2023-09-14 16:04:00 -0400

    Changed colour fill to continuous, for SE maps particularly

--- functions/map_trends.R
+++ functions/map_trends.R
@@ -1,8 +1,8 @@
 map_trends <- function(trends = trends,
                        variable = "trend",
                        plot_trend = TRUE,
-                          base_map_blank = base_map,
-                          title = "",
+                       base_map_blank = base_map,
+                       title = "",
                        legend_title = NULL,
                        zoom_out = 0.05,
                        add_base = TRUE,
@@ -18,24 +18,24 @@ map_trends <- function(trends = trends,
     trends <- trends[["trends"]] %>% 
       filter(region_type == "stratum") 
   }
-
+  
   if(facetgrid){
     if(!is.null(facet_2) &
        !is.null(facet_1)){
-    trends <- trends %>% 
-      rename_with(.,~gsub(facet_1,
-                          "fac1",
-                          .x)) %>% 
-      rename_with(.,~gsub(facet_2,
-                          "fac2",
-                          .x))
-    }else{
-    if(!is.null(facet_1)){
       trends <- trends %>% 
         rename_with(.,~gsub(facet_1,
                             "fac1",
-                            .x))  
-    }
+                            .x)) %>% 
+        rename_with(.,~gsub(facet_2,
+                            "fac2",
+                            .x))
+    }else{
+      if(!is.null(facet_1)){
+        trends <- trends %>% 
+          rename_with(.,~gsub(facet_1,
+                              "fac1",
+                              .x))  
+      }
       if(!is.null(facet_2)){
         trends <- trends %>% 
           rename_with(.,~gsub(facet_2,
@@ -44,42 +44,42 @@ map_trends <- function(trends = trends,
       }
     }
   }
- 
-  
-if(plot_trend){
-  breaks <- c(-7, -4, -2, -1, -0.5, 0.5, 1, 2, 4, 7)
-  labls <- c(paste0("< ", breaks[1]),
-             paste0(breaks[-c(length(breaks))],":", breaks[-c(1)]),
-             paste0("> ",breaks[length(breaks)]))
-  labls <- paste0(labls, " %")
-  
-  trends_plot <- trends %>% 
-    rename_with(.,~gsub(variable,"value",.x)) %>% 
-    mutate(t_plot = cut(value, breaks = c(-Inf, breaks, Inf),
-                        labels = labls,
-                        ordered_result = TRUE))
-  
-  pal <- setNames(
-    c("#a50026", "#d73027", "#f46d43", "#fdae61", "#fee090", "#ffffbf",
-      "#e0f3f8", "#abd9e9", "#74add1", "#4575b4", "#313695"),
-    levels(trends_plot$t_plot))
-  
-  start_year <- min(trends_plot$start_year)
-  end_year <- max(trends_plot$end_year)
   
   
-  if(!is.null(legend_title)){
-    l_title <- paste(legend_title,"\n",start_year,"-",end_year)
+  if(plot_trend){
+    breaks <- c(-7, -4, -2, -1, -0.5, 0.5, 1, 2, 4, 7)
+    labls <- c(paste0("< ", breaks[1]),
+               paste0(breaks[-c(length(breaks))],":", breaks[-c(1)]),
+               paste0("> ",breaks[length(breaks)]))
+    labls <- paste0(labls, " %")
     
-  }else{
-  l_title <- paste("Trend \n",start_year,"-",end_year)
-  }
-  if(length(unique(trends_plot$start_year)) > 1){
-    l_title <- "Trend"
-  }
+    trends_plot <- trends %>% 
+      rename_with(.,~gsub(variable,"value",.x)) %>% 
+      mutate(t_plot = cut(value, breaks = c(-Inf, breaks, Inf),
+                          labels = labls,
+                          ordered_result = TRUE))
+    
+    pal <- setNames(
+      c("#a50026", "#d73027", "#f46d43", "#fdae61", "#fee090", "#ffffbf",
+                 "#e0f3f8", "#abd9e9", "#74add1", "#4575b4", "#313695"),
+                 levels(trends_plot$t_plot))
+    
+    start_year <- min(trends_plot$start_year)
+    end_year <- max(trends_plot$end_year)
+    
+    
+    if(!is.null(legend_title)){
+      l_title <- paste(legend_title,"\n",start_year,"-",end_year)
+      
+    }else{
+      l_title <- paste("Trend \n",start_year,"-",end_year)
+    }
+    if(length(unique(trends_plot$start_year)) > 1){
+      l_title <- "Trend"
+    }
   }else{
     
-
+    
     trends_plot <- trends %>% 
       rename_with(.,~gsub(variable,"t_plot",.x)) 
     
@@ -91,7 +91,7 @@ if(plot_trend){
       l_title <- paste(legend_title,"\n",start_year,"-",end_year)
       
     }else{
-    l_title <- paste(variable,"\n",start_year,"-",end_year)
+      l_title <- paste(variable,"\n",start_year,"-",end_year)
     }
     if(length(unique(trends_plot$start_year)) > 1){
       if(!is.null(legend_title)){
@@ -106,7 +106,7 @@ if(plot_trend){
   
   
   
-
+  
   
   trend_map <- base_map_blank %>% 
     right_join(.,trends_plot,
@@ -124,11 +124,11 @@ if(plot_trend){
   
   if(plot_trend){
     plot_out <- plot_out +
-    scale_fill_manual(values = pal,
-                      guide = guide_legend(title = l_title))
+      scale_fill_manual(values = pal,
+                        guide = guide_legend(title = l_title))
   }else{
     plot_out <- plot_out +
-      scale_fill_viridis_c(guide = guide_legend(title = l_title))
+      scale_fill_continuous(type="viridis",name = l_title)
   }
   if(add_base){
     plot_out <-  plot_out+
@@ -138,8 +138,8 @@ if(plot_trend){
   
   if(facetgrid){
     if(is.null(facet_2)){
-  plot_out<- plot_out +
-    facet_grid(rows = vars(fac1))
+      plot_out<- plot_out +
+        facet_grid(rows = vars(fac1))
     }
     if(is.null(facet_1)){
       plot_out<- plot_out +
